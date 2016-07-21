4f31e812a4b194552ef47d09760718dd60bac203
Samantha Spensley <spenslsm@gmail.com>
2016-07-20 19:05:40 -0600

    initial commit old files

---
+++ main.js
@@ -0,0 +1,10 @@
+var niceThings = ['Your smile is contagious.', "You're a smart cookie.", 'I bet you make babies smile.', 'You have the best laugh.', 'I appreciate you.', 'You are the most perfect you there is.', "You're an awesome friend.", 'You deserve a hug right now.', "You've got all the right moves!", "You're all that and a super-size bag of chips.", "On a scale from 1 to 10, you're an 11.", 'If cartoon bluebirds were real, a bunch of them would be sitting on your shoulders singing right now.', "You're like sunshine on a rainy day.", 'I bet you sweat glitter.', 'Babies and small animals probably love you.', "You're even better than a unicorn, because you're real.", ]
+
+function generate() {
+  return Math.floor(Math.random() * niceThings.length);
+}
+console.log(generate());
+
+$('a').click(function(){
+  $('#result h1').html(niceThings[generate()]);
+})
